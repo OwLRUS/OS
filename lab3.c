a5b35625d8d0a4b24dacb3df2e09374b0a96bb46
Бриц Владислав <81351838+OwLRUS@users.noreply.github.com>
2023-12-18 16:50:07 +0700

    add labs 1,2,3

---
+++ lab3.c
@@ -0,0 +1,16 @@
+#include<linux/kernel.h>
+#include <linux/module.h>
+#include <linux/printk.h> 
+#include<linux/proc_fs.h>
+#include<linux/uaccess.h>
+#include <linux/version.h>
+
+int init_module(void) {
+pr_info("Welcome to Tomsk! Be healthy!\n");
+return 0;
+}
+void cleanup_module(void) {
+pr_info("Bye! Always dress according to the weather!\n");
+}
+
+MODULE_LICENSE("GPL");
