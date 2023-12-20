f8094bf7c50ccc4794d0b63bc195562b7c6c8d6e
Бриц Владислав <81351838+OwLRUS@users.noreply.github.com>
2023-12-20 16:24:46 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -5,6 +5,9 @@
 #include<linux/uaccess.h>
 #include <linux/version.h>
 
+#define procfs_name "tsu"
+static struct proc_dir_entry *our_proc_file = NULL;
+
 int init_module(void) {
 pr_info("Welcome to Tomsk! Be healthy!\n");
 return 0;
